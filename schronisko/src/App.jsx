53467930a28a80af3deb171fef5d855c22f61aab
Gosia Zaremba <gosia@szel.net>
2022-04-29 11:47:44 +0200

    themes created and applied to app

--- schronisko/src/App.jsx
+++ schronisko/src/App.jsx
@@ -2,29 +2,32 @@ import { Footer } from "./components/Footer/Footer";
 import { Topbar } from "./components/Topbar/Topbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as React from "react";
+import { themes } from "./themes";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Topbar />
-        <Routes>
-          <Route path="/" element={<div>Strona główna</div>} />
-          <Route
-            path="/wolontariat"
-            element={<div>Panel Wolontariusza logowanie i rejestracja</div>}
-          />
-          <Route
-            path="/psy"
-            element={<div>Lista psów dla wolonatriusza</div>}
-          />
-          <Route
-            path="/psy/:id"
-            element={<div>Strona konkretnego psa dla wolontariusza</div>}
-          />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <ThemeProvider theme={themes}>
+        <BrowserRouter>
+          <Topbar />
+          <Routes>
+            <Route path="/" element={<div>Strona główna</div>} />
+            <Route
+              path="/wolontariat"
+              element={<div>Panel Wolontariusza logowanie i rejestracja</div>}
+            />
+            <Route
+              path="/psy"
+              element={<div>Lista psów dla wolonatriusza</div>}
+            />
+            <Route
+              path="/psy/:id"
+              element={<div>Strona konkretnego psa dla wolontariusza</div>}
+            />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
---
+++ schronisko/src/themes/Themes.jsx
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export const themes = createTheme({
+  palette: {
+    primary: {
+      light: "#7177AD",
+      main: "#5e6390",
+      dark: "#4C5075",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#FF9EB5",
+      main: "#E28CA0",
+      dark: "#C97D8F",
+      contrastText: "#000",
+    },
+  },
+});
