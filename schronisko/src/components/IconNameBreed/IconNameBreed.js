6d5ecf0b3345cb66c31e42f4de1883be140f3e76
Piotr Piechowski <kontakt@piotrpiechowski.pl>
2022-04-29 13:58:53 +0200

    Added unstylished elements (icon, name, breed of dog) with no functionality.

---
+++ schronisko/src/components/IconNameBreed/IconNameBreed.js
@@ -0,0 +1,13 @@
+import PetsIcon from "@mui/icons-material/Pets";
+import Typography from "@mui/material/Typography";
+
+const IconNameBreed = () => {
+  return (
+    <>
+      <PetsIcon fontSize="large" />;<Typography variant="h3">Reksio</Typography>
+      <Typography variant="h5">Owczarek niemiecki</Typography>
+    </>
+  );
+};
+
+export default IconNameBreed;
