08606721362ddbf63f13effa5ad07bb8abd64989
Gosia Zaremba <gosia@szel.net>
2022-04-29 19:32:03 +0200

    fixed bugs with Logo component

--- schronisko/src/components/Logo/logo.jsx
+++ schronisko/src/components/Logo/logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
         width="150"
         height="80"
         viewBox="0 0 320 115.61542699574032"
-        class="css-1j8o68f"
+        className="css-1j8o68f"
       >
         <defs id="SvgjsDefs1001"></defs>
         <g
