672869c2f2cace3d842d0cb0669c2916f311da4b
Gosia Zaremba <gosia@szel.net>
2022-04-30 12:42:21 +0200

    dog profile photo added, css styles for links in navbar fixed

---
+++ schronisko/src/components/DogProfile/DogPhoto.jsx
@@ -0,0 +1,13 @@
+const DogPhoto = () => {
+  return (
+    <div className="profileDogPhoto">
+      <img
+        className="profileDogPhoto"
+        src="https://picsum.photos/id/237/300/400"
+        alt="dogphoto"
+      ></img>
+    </div>
+  );
+};
+
+export default DogPhoto;
