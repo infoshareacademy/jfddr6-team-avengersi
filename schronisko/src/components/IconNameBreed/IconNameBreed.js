dbc01a6348d4cde055f7b1c84ff5c2babc86e60d
Piotr Piechowski <kontakt@piotrpiechowski.pl>
2022-04-30 10:14:01 +0200

    A component with an icon, name and breed of the dog has been added. Component gets data after id, now id is const, not props.

--- schronisko/src/components/IconNameBreed/IconNameBreed.js
+++ schronisko/src/components/IconNameBreed/IconNameBreed.js
@@ -1,12 +1,46 @@
 import PetsIcon from "@mui/icons-material/Pets";
 import Typography from "@mui/material/Typography";
+import { doc, onSnapshot } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../../db";
 
 const IconNameBreed = () => {
+  const [dog, setDog] = useState([]);
+
+  const id = "egKLOmZq8r7pvNfNAdNC"; //props
+
+  const docRef = doc(db, "dogs", id);
+  onSnapshot(docRef, (doc) => {
+    const oneDog = {
+      name: doc.data().name,
+      breed: doc.data().breed,
+    };
+    setDog(oneDog);
+  });
+
+  //styled:
+
+  const mainStyle = {
+    display: "flex",
+    flexDirection: "row",
+  };
+
+  const iconStyle = {
+    display: "flex",
+    alignItems: "center",
+    padding: "0px 10px",
+  };
+
   return (
-    <>
-      <PetsIcon fontSize="large" />;<Typography variant="h3">Reksio</Typography>
-      <Typography variant="h5">Owczarek niemiecki</Typography>
-    </>
+    <div style={mainStyle}>
+      <div style={iconStyle}>
+        <PetsIcon fontSize="large" />
+      </div>
+      <div>
+        <Typography variant="h5">{dog.name}</Typography>
+        <Typography variant="body2">{dog.breed}</Typography>
+      </div>
+    </div>
   );
 };
 
