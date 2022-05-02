29ac8feb5bc3e683dff9d013a13bfe9c5f61a3b6
Piotr Piechowski <kontakt@piotrpiechowski.pl>
2022-05-02 18:55:01 +0200

    Added HomePage Alpha-version

--- schronisko/src/App.jsx
+++ schronisko/src/App.jsx
@@ -6,30 +6,34 @@ import * as React from "react";
 import { theme } from "./themes/Themes";
 import { ThemeProvider } from "@mui/material/styles";
 import { UploadPhoto } from "./components/AddDog/UploadPhoto";
+import HomePage from "./routes/HomePage";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <ResponsiveAppBar color="palette.primary" />
-        <Routes>
-          <Route path="/" element={<div>Strona główna</div>} />
-          <Route
-            path="/wolontariat"
-            element={<div>Panel Wolontariusza logowanie i rejestracja</div>}
-          />
-          <Route
-            path="/psy"
-            element={<div>Lista psów dla wolonatriusza</div>}
-          />
-          <Route
-            path="/psy/:id"
-            element={<div>Strona konkretnego psa dla wolontariusza</div>}
-          />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <BrowserRouter>
+    //     <ResponsiveAppBar color="palette.primary" />
+    //     <Routes>
+    //       <Route path="/" element={<div>Strona główna</div>} />
+    //       <Route
+    //         path="/wolontariat"
+    //         element={<div>Panel Wolontariusza logowanie i rejestracja</div>}
+    //       />
+    //       <Route
+    //         path="/psy"
+    //         element={<div>Lista psów dla wolonatriusza</div>}
+    //       />
+    //       <Route
+    //         path="/psy/:id"
+    //         element={<div>Strona konkretnego psa dla wolontariusza</div>}
+    //       />
+    //     </Routes>
+    //     <Footer />
+    //   </BrowserRouter>
+    // </ThemeProvider>
+    <>
+      <HomePage />
+    </>
   );
 }
 
