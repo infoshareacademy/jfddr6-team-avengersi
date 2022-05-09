1cc3023bf155b16ee024df725aab419bc11c01b0
Gosia Zaremba <gosia@szel.net>
2022-05-09 18:02:33 +0200

    app.jsx file fixed

--- schronisko/src/App.jsx
+++ schronisko/src/App.jsx
@@ -1,10 +1,36 @@
+import "./App.css";
 import { Footer } from "./components/Footer/Footer";
-// import { Topbar } from "./components/Topbar/Topbar";
+import ResponsiveAppBar from "./components/Topbar/ResponsiveAppBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as React from "react";
 import { theme } from "./themes/Themes";
 import { ThemeProvider } from "@mui/material/styles";
+import Slider from "./components/Slider/Slider";
 
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Slider />} />
+          <Route
+            path="/wolontariat"
+            element={<div>Panel Wolontariusza logowanie i rejestracja</div>}
+          />
+          <Route
+            path="/psy"
+            element={<div>Lista psów dla wolonatriusza</div>}
+          />
+          <Route
+            path="/psy/:id"
+            element={<div>Strona konkretnego psa dla wolontariusza</div>}
+          />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
