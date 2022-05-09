2291f6c476eb5033d3475b6d2670b2c9d5a9f333
Anna Pawlowska <96695890+AnnaPawlowskaa@users.noreply.github.com>
2022-05-09 22:38:21 +0200

    comment in progress

---
+++ schronisko/src/components/Comment/AddComment.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { Container } from "@mui/material";
+import { TextField, Box } from "@mui/material";
+import { useState } from "react";
+import { db } from "../../db";
+import { updateDoc, doc, collection } from "firebase/firestore";
+
+const dog = {
+  id: "25f6188c-1f41-4894-81a2-ecf376ec0b9f",
+  comment: "utykał na spacerze",
+};
+
+export default function AddComment() {
+  const [commentValue, setCommentValue] = useState(dog.comment);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleClickEdit = React.useCallback(() => {
+    setEditMode((prev) => !prev);
+  }, []);
+
+  const submitComment = React.useCallback(async () => {
+    const result = await updateDoc(doc(db, "dogs", dog.id), {
+      commentValue,
+    });
+
+    // const result = await updateDoc(
+    //   doc(db, "dogs", dog.id).collection("comments").add(commentValue)
+    // );
+
+    console.log(result);
+    setCommentValue(commentValue);
+
+    setEditMode(false);
+  }, [commentValue]);
+
+  const handleSubmit = React.useCallback(
+    (e) => {
+      e.preventDefault();
+      submitComment();
+      console.log("Ania");
+    },
+    [submitComment]
+  );
+
+  const handleChangeComment = React.useCallback((e) => {
+    setCommentValue(e.target.value);
+  }, []);
+
+  return (
+    <Container>
+      <Box>
+        <Button onClick={handleClickEdit}>
+          {editMode ? <>Stop Editing</> : <>Start Editing</>}
+        </Button>
+      </Box>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          label="Komentarz"
+          variant="outlined"
+          color="primary"
+          fullWidth
+          multiline
+          rows={7}
+          value={commentValue}
+          disabled={!editMode}
+          onChange={handleChangeComment}
+        />
+        <Box mt={1}>
+          <Button variant="contained" color="primary" type="submit">
+            Ok
+          </Button>
+        </Box>
+      </form>
+    </Container>
+  );
+}
