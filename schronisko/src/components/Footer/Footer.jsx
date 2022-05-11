5b92906896b2eedb2bb2f308d2515fc9f95d7480
Gosia Zaremba <gosia@szel.net>
2022-05-11 18:49:21 +0200

    footer

--- schronisko/src/components/Footer/Footer.jsx
+++ schronisko/src/components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ export const Footer = () => {
         sx={{
           gridColumn: "1/1",
           display: "grid",
-          gridTemplateColumns: "repeat(2, 1fr)",
+          gridTemplateColumns: "repeat(3, 1fr)",
           gridTemplateRows: "repeat(3, 1fr)",
           alignItems: "center",
           marginTop: "10px",
@@ -157,19 +157,30 @@ export const Footer = () => {
         <Copyright />
       </Container>
       <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ gridColumn: "3/3", alignSelf: "end" }}
+        sx={{
+          gridColumn: "3/3",
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          justifyItems: "center",
+          alignItems: "center",
+        }}
       >
-        <Typography variant="body2" color="#fff">
+        <Typography
+          variant="body2"
+          color="#fff"
+          sx={{
+            gridColumn: "2/2",
+          }}
+        >
           Wpłać darowiznę{" "}
         </Typography>
         <Link
           href="https://www.paypal.com/donate/?hosted_button_id=P8GCJVPGGBQ4Y"
           target="_blank"
           rel="noopener"
+          sx={{
+            gridColumn: "2/2",
+          }}
         >
           <img
             style={{ borderRadius: "20px" }}
@@ -183,119 +194,3 @@ export const Footer = () => {
     </Box>
   );
 };
-
-// export const Footer = () => {
-//   return (
-//     <footer>
-//       <Box
-//         px={{ xs: 1, sm: 3, md: 4 }}
-//         py={{ xs: 1, sm: 3, md: 4 }}
-//         bgcolor="text.primary"
-//         color="primary.light"
-//       >
-//         <Container>
-//           <Typography variant="body2" gutterBottom>
-//             <Grid
-//               container
-//               spacing={8}
-//               justifyContent="space-between"
-//               alignItems="center"
-//             >
-//               <Grid item xs={12} sm={4}>
-//                 <Box>Aplikacja stworzona przez Avengers Team</Box>
-//                 <Box>
-//                   <img
-//                     src={GitHubImg}
-//                     width="12"
-//                     height="12"
-//                     alt="github icon"
-//                   ></img>
-//                   <Link
-//                     href="https://github.com/AnnaPawlowskaa"
-//                     color="primary.contrastText"
-//                     underline="hover"
-//                   >
-//                     Anna Pawłowska
-//                   </Link>
-//                 </Box>
-//                 <Box>
-//                   <img
-//                     src={GitHubImg}
-//                     width="12"
-//                     height="12"
-//                     alt="github icon"
-//                   ></img>
-//                   <Link
-//                     href="https://github.com/ppiechowski"
-//                     color="primary.contrastText"
-//                     underline="hover"
-//                   >
-//                     Piotr Piechowski
-//                   </Link>
-//                 </Box>
-//                 <Box>
-//                   <img
-//                     src={GitHubImg}
-//                     width="12"
-//                     height="12"
-//                     alt="github icon"
-//                   ></img>
-//                   <Link
-//                     href="https://github.com/MartaKatUrbaniak"
-//                     color="primary.contrastText"
-//                     underline="hover"
-//                   >
-//                     Marta Urbaniak
-//                   </Link>
-//                 </Box>
-//                 <Box>
-//                   <img
-//                     src={GitHubImg}
-//                     width="12"
-//                     height="12"
-//                     alt="github icon"
-//                   ></img>
-//                   <Link
-//                     href="https://github.com/GosiaZaremba"
-//                     color="primary.contrastText"
-//                     underline="hover"
-//                   >
-//                     Gosia Zaremba
-//                   </Link>
-//                 </Box>
-//               </Grid>
-//               <Grid item justifySelf="end">
-//                 <Box display="flex" flexDirection="column" alignItems="center">
-//                   <Typography variant="body2">Wpłać darowiznę </Typography>
-//                   <a
-//                     href="https://www.paypal.com/donate/?hosted_button_id=P8GCJVPGGBQ4Y"
-//                     color="primary.contrastText"
-//                   >
-//                     <img
-//                       style={{ borderRadius: "20px" }}
-//                       width="90"
-//                       height="90"
-//                       src={DonationImg}
-//                       alt="french bulldog carries donate box"
-//                     />
-//                   </a>
-//                 </Box>
-//               </Grid>
-//             </Grid>
-//             <Box
-//               color="primary.contrastText"
-//               textAlign="center"
-//               pt={{ xs: 1, sm: 0 }}
-//               pb={{ xs: 1, sm: 0 }}
-//             >
-//               <Typography variant="caption" display="block" gutterBottom>
-//                 ALL RIGHTS RESERVED &reg; {new Date().getFullYear()} InfoShare
-//                 Academy
-//               </Typography>
-//             </Box>
-//           </Typography>
-//         </Container>
-//       </Box>
-//     </footer>
-//   );
-// };
