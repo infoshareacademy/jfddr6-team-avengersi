560531289eaff24da8722e733be28157ab662490
Marta Urbaniak <martakaturbaniak@gmail.com>
2022-05-12 16:29:16 +0200

    osobne widoki listy uzależnione od zalogowania

--- schronisko/src/App.jsx
+++ schronisko/src/App.jsx
@@ -9,14 +9,28 @@ import Slider from "./components/Slider/Slider";
 import { AddDogForm } from "./components/AddDog/AddDogForm";
 import SingleDogViewWithEdition from "./components/SingleDogViewWithEdition/SingleDogViewWithEdition";
 import { DogList } from "./Routes/DogList";
+import DogsListForClients from "./components//DogsList/DogsListForClients";
 import { Home } from "./Routes/Home";
 import { Dashboard } from "./Routes/Dashboard";
 import moment from "moment";
 import pl from "moment/locale/pl";
 import { ClientsForm } from "./components/ClientsForm/ClientsForm";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./db";
+import { useState } from "react";
 
 function App() {
   moment.locale("pl");
+
+  const [isAuth, setIsAuth] = useState("");
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsAuth(user.email);
+    } else {
+      setIsAuth(false);
+    }
+  });
 
   return (
     <ThemeProvider theme={theme}>
@@ -25,13 +39,17 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dogs" element={<DogList />} />
+          <Route
+            path="/dogs"
+            element={isAuth ? <DogList /> : <DogsListForClients />}
+          />
           <Route path="/dog/:id" element={<SingleDogViewWithEdition />} />
           <Route path="/addDog" element={<AddDogForm />} />
           <Route path="/adopt" element={<ClientsForm />} />
         </Routes>
         <Footer />
       </BrowserRouter>
+      DogsListForClients
     </ThemeProvider>
   );
 }
